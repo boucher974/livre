75dce3343b2e83b6571f8dfa6334886356d3490a
boucher974 <120424989+boucher974@users.noreply.github.com>
2023-02-26 08:25:52 +0100

    Add files via upload

---
+++ Doritos/description.lua
@@ -0,0 +1,88 @@
+livery = {
+	{"ext1", 0 ,"M2KC_1T_GUSTO",false};
+--	{"ext1", 13 ,"M2KC_1T_RoughMet",false};
+	
+	{"ext2", 0 ,"M2KC_2T_GUSTO",false};
+	{"ext6", 0 ,"M2KC_6T",false};
+	{"pilot_M2KC_helmet", 0 ,"Pilot_M2KC_helmet",false};
+	{"pilot_M2KC", 0 ,"Pilot_M2KC",false};
+	{"pilot_M2KC_patch", 0 ,"Pilot_M2KC_patch",false};
+
+	{"FUSE_NUMBER1_001", 0 ,"M2KC_1T_GUSTO",false};
+	{"FUSE_NUMBER1_001", DECAL ,"empty",false};
+
+	{"FUSE_NUMBER2_001", 0 ,"M2KC_1T_GUSTO",false};
+	{"FUSE_NUMBER2_001", DECAL ,"empty",false};
+
+	{"FUSE_NUMBER2_010", 0 ,"M2KC_1T_GUSTO",false};
+	{"FUSE_NUMBER2_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER1_001", 0 ,"M2KC_2T_GUSTO",false};
+	{"TAIL_NUMBER1_001", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER1_010", 0 ,"M2KC_2T_GUSTO",false};
+	{"TAIL_NUMBER1_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER1_100", 0 ,"M2KC_2T_GUSTO",false};
+	{"TAIL_NUMBER1_100", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER2_001", 0 ,"M2KC_2T_GUSTO",false};
+	{"TAIL_NUMBER2_001", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER2_010", 0 ,"M2KC_2T_GUSTO",false};
+	{"TAIL_NUMBER2_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER2_100", 0 ,"M2KC_2T_GUSTO",false};
+	{"TAIL_NUMBER2_100", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER3_001", 0 ,"M2KC_2T_GUSTO",false};
+	{"TAIL_NUMBER3_001", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER3_010", 0 ,"M2KC_2T_GUSTO",false};
+	{"TAIL_NUMBER3_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER3_100", 0 ,"M2KC_2T_GUSTO",false};
+	{"TAIL_NUMBER3_100", DECAL ,"empty",false};
+
+	{"NOSE_NUMBER_001", 0 ,"M2KC_1T_GUSTO",false};
+	{"NOSE_NUMBER_001", DECAL ,"empty",false};
+
+	{"NOSE_NUMBER_010", 0 ,"M2KC_1T_GUSTO",false};
+	{"NOSE_NUMBER_010", DECAL ,"empty",false};
+
+	{"NOSE_NUMBER_100", 0 ,"M2KC_1T_GUSTO",false};
+	{"NOSE_NUMBER_100", DECAL ,"empty",false};
+
+	{"WINGR_NUMBER_001", 0 ,"M2KC_2T_GUSTO",false};
+	{"WINGR_NUMBER_001", DECAL ,"empty",false};
+
+	{"WINGR_NUMBER_010", 0 ,"M2KC_2T_GUSTO",false};
+	{"WINGR_NUMBER_010", DECAL ,"empty",false};
+
+	{"WINGR_NUMBER_100", 0 ,"M2KC_2T_GUSTO",false};
+	{"WINGR_NUMBER_100", DECAL ,"empty",false};
+
+	{"WINGL_NUMBER_001", 0 ,"M2KC_1T_GUSTO",false};
+	{"WINGL_NUMBER_001", DECAL ,"empty",false};
+
+	{"WINGL_NUMBER_010", 0 ,"M2KC_1T_GUSTO",false};
+	{"WINGL_NUMBER_010", DECAL ,"empty",false};
+
+	{"WINGL_NUMBER_100", 0 ,"M2KC_1T_GUSTO",false};
+	{"WINGL_NUMBER_100", DECAL ,"empty",false};
+	
+	{"M2000C-19", 0 ,"M2000C_19C_A",true};
+	{"M2000C-20", 0 ,"M2000C_20C_A",true};
+	{"M2000C-22", 0 ,"M2000C_22C_CAMB",true};
+}
+name = "Doritos"
+countries = {"CZE","USA","RUS","FRA","UKR","SPN","NETH","TUR","BEL","GER","NOR","CAN","POL","DEN","UK","GRG","ISR","ABH","RSO","ITA","IRA",}
+
+custom_args = 
+{
+    [308] = 0.15,
+    [316] = 0.15,
+
+}
+
+-- livery by Sphimx (@sphimx_livery on instagram) 2021
